057485700333af938e87de1c8fba679d1cba7150
Saxsori <saxxxsori@gmail.com>
2024-01-17 13:30:15 +0400

    adding the package for ex09 module 0

---
+++ src/python-0/ex09/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='ft_package',
+    version='0.0.1',
+    author='Abrar Aljaberi',
+    author_email='aaljaber@student.42abudhabi.ae',
+    description='A small ft package',
+	packages=['ft_package'],
+    location= '.',
+	license='MIT',
+    zip_safe=False
+)
--- src/python-0/ex09/tester.py
+++ src/python-0/ex09/tester.py
@@ -1,3 +1,3 @@
-from ft_package import count_in_list
+from ft_package.count_in_list import count_in_list
 print(count_in_list(["toto", "tata", "toto"], "toto")) # output: 2
 print(count_in_list(["toto", "tata", "toto"], "tutu")) # output: 0
